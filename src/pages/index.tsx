be1b12a7dc99a19efc1c8b5ebc295705610487fb
Fernando Butzke <nandoproject209@gmail.com>
2022-05-22 20:52:42 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Donapets</title>
+      </Head>
+      <h1>Teste</h1>
+    </>
+  )
+}
